923ee9abd2355eb40ddf0ebf31bdc6ea96cd32da
Anurag Kumar <anuragabcr@gmail.com>
2023-07-31 23:10:48 +0530

    api counter UI added

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -14,6 +14,7 @@ import {
   Code,
   Settings,
 } from "lucide-react";
+import FreeCounter from "./FreeCounter";
 
 const routes = [
   {
@@ -61,7 +62,11 @@ const routes = [
 
 const monstserrat = Montserrat({ weight: "600", subsets: ["latin"] });
 
-const Sidebar = () => {
+interface SidebarProps {
+  apiLimitCount: number;
+}
+
+const Sidebar = ({ apiLimitCount = 0 }: SidebarProps) => {
   const pathname = usePathname();
   return (
     <div className="space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white">
@@ -94,6 +99,7 @@ const Sidebar = () => {
           ))}
         </div>
       </div>
+      <FreeCounter apiLimitCount={apiLimitCount} />
     </div>
   );
 };
